d103d4c0a898676e4f3b4e483b1a4c5675a37fcc
nurgnc <gencbedriyenur@gmail.com>
2022-01-06 00:45:22 +0300

    search bar created, search function added and debounced imported

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import Products from './components/Products/Products';
-
+import './index.css';
 function App() {
   return (
     <div className="App">
--- src/components/Search/SearchBar.jsx
+++ src/components/Search/SearchBar.jsx
@@ -1,4 +1,19 @@
-function SearchBar() {
-  return <h1>SearchBar</h1>;
+import {AiOutlineSearch} from 'react-icons/ai';
+
+
+function SearchBar({value, onChangeText}) {
+
+  return (
+    <div className="search-container">
+      {/* <AiOutlineSearch /> */}
+      <input
+        className='sbx-custom__input'
+        type="text"
+        value={value}
+        onChange={onChangeText}
+        placeholder={" Search beer by name" }
+      />
+    </div>
+  );
 }
 export default SearchBar;
