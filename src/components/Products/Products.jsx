feba4d6983ec38962d7781690c131eb072465e86
nurgnc <gencbedriyenur@gmail.com>
2022-01-07 19:45:38 +0300

    pagination updated

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -5,6 +5,7 @@ import baseURL from "../../api";
 import ProductCard from "./ProductCard";
 import SearchBar from "../Search/SearchBar";
 import ReactPaginate from "react-paginate";
+import Pagination from "./Pagination";
 
 function Products() {
   const [data, setData] = useState([]);
@@ -12,7 +13,6 @@ function Products() {
 
   const beersPerPage = 6;
   const pagesVisited = pageNumber * beersPerPage;
-  // const displayBeers = data.slice(pagesVisited, pagesVisited + beersPerPage).map(item => (<ProductCard name={item.name} key={item.id} img={item.image_url} date={item.first_brewed} />)
 
   useEffect(() => {
     fetch(baseURL)
@@ -21,12 +21,6 @@ function Products() {
         setData(json);
       });
   }, []);
-  console.log(data.length / beersPerPage);
-
-  const pageCount = data.length / beersPerPage;
-  const changePage = ({ selected }) => {
-    setPageNumber(selected);
-  };
 
   return (
     <>
@@ -34,7 +28,6 @@ function Products() {
         <SearchBar data={data} />
         <hr />
         <div className="row row-cols-1 row-cols-sm-1 row-cols-md-1 row-cols-lg-3">
-          <h1>Products:</h1>
           {data
             .slice(pagesVisited, pagesVisited + beersPerPage)
             .map((item, index) => (
@@ -46,16 +39,10 @@ function Products() {
               />
             ))}
         </div>
-        <ReactPaginate
-          previousLabel={"Previous"}
-          nextLabel={"Next"}
-          pageCount={pageCount}
-          onPageChange={changePage}
-          containerClassName={"paginationBttns"}
-          previousLinkClassName={"previousBttn"}
-          nextLinkClassName={"NextBttn"}
-          disabledClassName={"paginationDisabled"}
-          activeClassName={"paginationActive"}
+        <Pagination
+          beersPerPage={beersPerPage}
+          data={data}
+          setPageNumber={setPageNumber}
         />
       </div>
     </>
